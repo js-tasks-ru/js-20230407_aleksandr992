565e01dc5083bf623fe353fc766a62587bfaf815
amatul <rusfuse@gmail.com>
2023-04-13 15:54:47 +0300

    написал функцию сортировки массива

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -4,6 +4,12 @@
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
-export function sortStrings(arr, param = 'asc') {
+export function sortStrings(arr, param) {
 
+  const sortedArray = arr.slice(0).sort((a, b) => a.localeCompare(b, 'ru-RU-u-kf-upper', { sensitivity: 'case' }));
+
+  if (param === 'desc') {
+    return sortedArray.reverse();
+  }
+  return sortedArray;
 }
